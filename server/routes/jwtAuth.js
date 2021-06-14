c4483e3cffcfbf31f43d32d648b18272c0f73867
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-06-14 14:51:53 -0400

    added login functionality

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const pool = require("../db.js");
 const bcrypt = require("bcrypt");
 const jwtGenerator = require("../utils/jwtGenerator");
-const { JsonWebTokenError } = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
   try {
@@ -25,6 +24,30 @@ router.post("/register", async (req, res) => {
     );
     const token = jwtGenerator(newUser.rows[0].user_id);
 
+    res.json({ token });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await pool.query("SELECT * FROM users WHERE user_email=$1", [
+      email,
+    ]);
+    if (user.rows.length === 0) {
+      return res.status(401).send("no user");
+    }
+    const validPassword = await bcrypt.compare(
+      password,
+      user.rows[0].user_password
+    );
+    if (!validPassword) {
+      return res.status(401).json("Incorrect Password");
+    }
+    const token = jwtGenerator(user.rows[0].user_id);
     res.json({ token });
   } catch (err) {
     console.log(err.message);
