2f7e1894efacea5fa44d6e68da028bf4f02fa5e9
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-06-15 14:01:26 -0400

    dashboard

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,6 +1,33 @@
-import { Fragment } from "react";
+import { Fragment, useState, useEffect} from "react";
 
-export const Dashboard = () => {
-  return <Fragment></Fragment>;
+export const Dashboard = ({ setAuth }) => {
+  const [name, setName] = useState("");
+  async function getName() {
+    try {
+      const response = await fetch("http://localhost:5000/dashboard", {
+        method: "GET",
+        headers: { token: localStorage.token },
+      });
+      const parseRes = await response.json()
+      console.log(parseRes)
+      setName(parseRes.user_name)
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  const logout = (e) => {
+    e.preventDefault()
+    localStorage.removeItem("token")
+    setAuth(false)
+  }
+  useEffect(() => {
+    getName();
+  });
+  return (
+    <Fragment>
+      <h1>Dashbaord {name}</h1>
+      <button onClick={e => logout(e)}className="btn btn-primary">Logout</button>
+    </Fragment>
+  );
 };
 export default Dashboard;
