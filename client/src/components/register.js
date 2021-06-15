ba79b72b0322fc734196b8fc007123b3b1c655e4
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-06-15 13:36:47 -0400

    login and register

--- client/src/components/register.js
+++ client/src/components/register.js
@@ -1,28 +1,66 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+import {Link} from "react-router-dom"
 
-const Register = () => {
+const Register = ({ setAuth }) => {
+  const [inputs, setInputs] = useState({
+    email: "pig",
+    password: "",
+    name: "",
+  });
+  const { email, password, name } = inputs;
+  const onChange = (e) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  };
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    const body = { email, password, name };
+    try {
+      const response = await fetch("http://localhost:5000/auth/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      const parseRes = await response.json();
+      localStorage.setItem("token", parseRes.token);
+      setAuth(true);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   return (
     <Fragment>
       <h1 className="text-center my-5">Register</h1>
-      <input
-        type="email"
-        name="email"
-        placeholder="email"
-        className="form-control my-3"
-      />
-      <input
-        type="text"
-        name="name"
-        placeholder="name"
-        className="form-control my-3"
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="password"
-        className="form-control my-3"
-      />
-      <button className="btn btn-success btn-block">Submit</button>
+      <form onSubmit={onSubmitForm}>
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          className="form-control my-3"
+          value={email}
+          onChange={(e) => onChange(e)}
+        />
+        <input
+          type="text"
+          name="name"
+          placeholder="name"
+          className="form-control my-3"
+          value={name}
+          onChange={(e) => onChange(e)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          className="form-control my-3"
+          value={password}
+          onChange={(e) => onChange(e)}
+        />
+        <button className="btn btn-success btn-block">Submit</button>
+      </form>
+      <Link to="/login">Login</Link>
+
     </Fragment>
   );
 };
