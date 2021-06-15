d8895ee9a2a51794202102d048eef2d947e102be
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-06-15 11:14:37 -0400

    router setup

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,34 @@ import {
   Redirect,
 } from "react-router-dom";
 
+import Dashboard from "./components/dashboard";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
 function App() {
   return (
     <Fragment>
-      <h1>hi</h1>
+      <Router>
+        <div className="container">
+          <Switch>
+            <Route
+              exact
+              path="/login"
+              render={(props) => <Login {...props} />}
+            ></Route>
+            <Route
+              exact
+              path="/register"
+              render={(props) => <Register {...props} />}
+            ></Route>
+            <Route
+              exact
+              path="/dashboard"
+              render={(props) => <Dashboard {...props} />}
+            ></Route>
+          </Switch>
+        </div>
+      </Router>
     </Fragment>
   );
 }
