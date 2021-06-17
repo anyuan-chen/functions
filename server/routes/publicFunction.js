eb7957685f7515ad37011197d43a447fe4fe3a9d
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-06-17 10:15:16 -0400

    post route for functions

--- server/routes/publicFunction.js
+++ server/routes/publicFunction.js
@@ -1,23 +1,31 @@
 const express = require("express");
+const pool = require("../db.js");
 
 const router = express.Router();
 
-router.get("/", async(req,res) => {
+router.get("/", async (req, res) => {});
 
-})
-
-router.post("/", async(req,res) => {
-    if (req.files === null){
-        return res.status(400).json("Bad Request")
+router.post("/add", async (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json("Bad Request");
+  }
+  const file = req.files.file;
+  const { title, description, user_id } = req.body;
+  console.log(req.body)
+  const fileLocation = `/users/andrewchen/code/functionsdata/${file.name}`;
+  file.mv(fileLocation, (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
     }
-    const file = req.files.file;
-    file.mv(`/users/andrewchen/code/functionsdata/${file.name}`, err => {
-        if (err){
-            console.error(err)
-            return res.status(500).send(err)
-        }
-        res.json({fileName: file.name, filePath : `../files/${file.name}`})
-    })
-})
+    //res.json({ fileName: file.name, filePath: `../files/${file.name}` });
+  });
+
+  const newFunction = await pool.query(
+    "INSERT INTO functions (title, file_name, description, user_id) VALUES ($1, $2, $3, $4) RETURNING * ",
+    [title, fileLocation, description, user_id]
+  );
+  res.json(newFunction.rows[0]);
+});
 
 module.exports = router;
